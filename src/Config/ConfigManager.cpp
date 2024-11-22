bcaf593543bfca89c6ec7916b2c31812aa7d3897
zzidy <0405zidi@gmail.com>
2024-11-23 02:22:40 +0800

    编写config模块

--- src/Config/ConfigManager.cpp
+++ src/Config/ConfigManager.cpp
@@ -2,6 +2,14 @@
 
 namespace Config {
 
+ConfigManager::ConfigManager() { m_mapCfg.clear(); }
+
 ConfigManager::~ConfigManager() {}
+
+bool ConfigManager::AddConfig(const std::string& sName,
+                              const std::string& sPath) {
+  m_mapCfg[sName] = std::make_shared<ConfigBase>(sName, sPath);
+  return false;
+}
 
 }  // namespace Config
